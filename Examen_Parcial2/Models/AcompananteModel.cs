956b40dba8e12e8d183a4b18513885edc4185cfd
asdru333 <asdrubal.villegas.molina@hotmail.com>
2021-11-29 07:31:23 -0600

    Modelos creados

---
+++ Examen_Parcial2/Models/AcompananteModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace Examen_Parcial2.Models
+{
+    public class AcompananteModel
+    { 
+        [Display(Name = "Unidades")]
+        [Required(ErrorMessage = "Es necesario que ingrese de cuantas unidades esta compuesto el acompañante")]
+        public int unidades { get; set; }
+    }
+}
---
+++ Examen_Parcial2/Models/BebidaModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace Examen_Parcial2.Models
+{
+    public class BebidaModel
+    {
+        [Display(Name = "Categoria")]
+        [Required(ErrorMessage = "Es necesario que ingrese la categoria de la bebida")]
+        public string categoriaBebida { get; set; }
+
+        [Display(Name = "Litros")]
+        [Required(ErrorMessage = "Es necesario que ingrese los litros que contiene la bebida")]
+        public double litros { get; set; }
+    }
+}
