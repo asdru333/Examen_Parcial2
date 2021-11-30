36448ef1e5464d9e5a8bf08bb2cf51bde5dee33e
asdru333 <asdrubal.villegas.molina@hotmail.com>
2021-11-30 12:56:45 -0600

    combo model agregado

---
+++ Examen_Parcial2/Models/ComboModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace Examen_Parcial2.Models
+{
+    public class ComboModel: ComestibleModel
+    { 
+        [Display(Name = "Pizza")]
+        [Required(ErrorMessage = "Es necesario que ingrese el nombre de la pizza")]
+        public string pizza{ get; set; }
+
+        [Display(Name = "Bebida")]
+        [Required(ErrorMessage = "Es necesario que ingrese el nombre de la pizza")]
+        public string bebida { get; set; }
+
+        [Display(Name = "Acompanante")]
+        [Required(ErrorMessage = "Es necesario que ingrese el nombre de la pizza")]
+        public string acompanante { get; set; }
+    }
+}
