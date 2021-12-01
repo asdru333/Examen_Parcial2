cc5f13c4bc6608212c3c9e68fa1dd52d24b011c3
asdru333 <asdrubal.villegas.molina@hotmail.com>
2021-11-30 18:34:38 -0600

    modelo y handler de pedidos agregados

---
+++ Examen_Parcial2/Handlers/PedidoHandler.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System;
+using System.Web.Mvc;
+using Examen_Parcial2.Models;
+
+namespace Examen_Parcial2.Handlers
+{
+    public class PedidoHandler: BaseDatosHandler
+    {
+        public List<PedidoModel> obtenerPedidos()
+        {
+            string consulta = "SELECT * FROM Pedido;";
+            List<PedidoModel> pedido = new List<PedidoModel>();
+            DataTable tabla = leerBaseDeDatos(consulta);
+            foreach (DataRow columna in tabla.Rows)
+            {
+                pedido.Add(
+                    new PedidoModel
+                    {
+                        ID = Convert.ToInt32(columna["pedidoIDPK"]),
+                        nombreComestible = Convert.ToString(columna["precio"]),
+                        nombreCliente = Convert.ToString(columna["pizzaFK"]),
+                        apellidoCliente = Convert.ToString(columna["bebidaFK"]),
+                        direccion = Convert.ToString(columna["direccion"]),
+                        precio = Convert.ToDouble(columna["precio"])
+                    } );
+            }
+            return pedido;
+        }
+     
+        public bool agregarPedido(PedidoModel pedido)
+        {
+            string consulta = "INSERT INTO Comestible ( nombreComestibleFK, nombreCliente, apellidoCliente, direccion, precio ) "
+                + "VALUES ( @comestible, @nombreCliente, @apellidoCliente, @direccion, @precio );";
+
+            Dictionary<string, object> valoresParametros = new Dictionary<string, object> {
+                {"@comestible", pedido.nombreComestible },
+                {"@nombreCliente", pedido.nombreCliente },
+                {"@apellidoCliente", pedido.apellidoCliente },
+                {"@direccion", pedido.direccion },
+                {"@precio", pedido.precio }
+            };
+
+            return (insertarEnBaseDatos(consulta, valoresParametros));
+        }
+    }
+}
+
---
+++ Examen_Parcial2/Models/PedidoModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace Examen_Parcial2.Models
+{
+    public class PedidoModel
+    {
+        [Display(Name = "ID")]
+        public int ID { get; set; }
+
+        [Display(Name = "pedido")]
+        public string nombreComestible { get; set; }
+
+        [Display(Name = "Su nombre")]
+        [Required(ErrorMessage = "Es necesario que ingrese su nombre")]
+        public string nombreCliente { get; set; }
+
+        [Display(Name = "Su apellido(s)")]
+        [Required(ErrorMessage = "Es necesario que ingrese su apellido")]
+         public string apellidoCliente { get; set; }
+
+        [Display(Name = "Dirección")]
+        [Required(ErrorMessage = "Es necesario que ingrese su dirección")]
+         public string direccion { get; set; }
+
+        [Display(Name = "Precio")]
+        public double precio { get; set; }
+    }
+}
